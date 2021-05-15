2296af2952e3357487378f5234fe328741213021
TOMER-D <tomer_max@hotmail.com>
2021-05-15 20:05:15 +0300

    update insurance detail js
    add validation to insurance detail page

--- FILES/insurance_details.js
+++ FILES/insurance_details.js
@@ -11,19 +11,8 @@ function validate(val)
     pre_ins_comp = document.getElementById("pre_ins_comp"); // 9
     comment = document.getElementById("comment"); // 10
     
+    requirements = ""; 
     
-    fname_flag = true;
-    lname_flag = true;
-    email_flag = true;
-    mobile_flag = true;
-    social_num_flag = true;
-    ins_amount_flag = true;
-    pre_ins_num_flag = true;
-    pre_ins_id_flag = true;
-    pre_ins_comp_flag = true;
-    comment_flag = true;
-    
-
 /*       FIRST NAME           */
 
     if(val=="fname" || val == "final_check") 
@@ -31,163 +20,195 @@ function validate(val)
         if(fname.value == "") 
         {
             fname.style.borderColor = "red";
-            fname_flag = false;
+            requirements += "fil the first name field\n";
+
         }
         else 
         {
             fname.style.borderColor = "green";
-            fname_flag = true;
+
         }
     }
 
 /*       LAST NAME           */
 
-    if(lname == "lname" || val== "final_check") {
+    if(val == "lname" || val== "final_check") 
+    {
         if(lname.value == "") 
         {
             lname.style.borderColor = "red";
-            lname_flag= false;
+            requirements += "fil the last name field\n";
+
         }
         else 
         {
             lname.style.borderColor = "green";
-            lname_flag = true;
+
         }
     }
 
 /*       EMAIL NAME           */
 
-    if(val == "email" || val=="final_check") {
+    if(val == "email" || val=="final_check") 
+    {
+        email_requst = "";
         if(email.value == "") 
+            email_requst+= "fill the email field\n";
+
+        str = email.value;
+        shindex = str.indexOf('@',0);
+        if (shindex != -1)
+            shindex = str.indexOf('.',shindex);
+        if (shindex == -1 )
+            email_requst+= "email not correct\n";
+        if(email_requst == "")
         {
-            email.style.borderColor = "red";
-            email_flag = false;
+            email.style.borderColor = "green";
+            
         }
         else 
         {
-            email.style.borderColor = "green";
-            email_flag = true;
+            email.style.borderColor = "red";
         }
+        requirements += email_requst;
     }
 
 /*       MOBILE           */
 
-    if(mobile == "mob" || val=="final_check") {
+    if(val == "mob" || val=="final_check") 
+    {
         if(mobile.value == "") {
             mobile.style.borderColor = "red";
-            mobile_flag = false;
+            requirements += "fil the mobile number field\n";
+
         }
         else {
             mobile.style.borderColor = "green";
-            mobile_flag = true;
+
         }
     }
 
 /*       SOCIAL NUMBER           */
 
-    if(social_num == "social_num" || val==0) 
+    if(val === 'social_num' || val === "final_check") 
     {
-        if(social_num.value == "") 
+        social_num_requst = "";
+        
+        if(social_num.value === "")
+            social_num_requst += "fill the social number field\n";
+
+        my_var = social_num.value;
+        for(var i = 0 ; i < my_var.length ;i++)
         {
-            social_num.style.borderColor = "red";
-            social_num_flag = false;
+            if (! Number.isInteger(my_var.charAt(i) - '0') )
+            {
+                social_num_requst += "social number has to be only numeric";
+                break;
+            }
         }
-        else 
-        {
+
+        if(social_num.value.length != 9)
+            social_num_requst += "social number has to be 9 digits";
+        
+        
+        if(social_num_requst === "") {
             social_num.style.borderColor = "green";
-            social_num_flag = true;
+        } else { 
+            social_num.style.borderColor = "red";
         }
+        
+        requirements += social_num_requst;
     }
 
 /*       INSURANCE AMOUNT           */
 
-    if(ins_amount == "ins_amount" || val==0) {
+    if(val == "ins_amount" || val==0) 
+    {
         if(ins_amount.value == "") 
         {
             ins_amount.style.borderColor = "red";
-            ins_amount_flag = false;
+            requirements += "fil the insurance amount field\n";
+
         }
         else 
         {
             ins_amount.style.borderColor = "green";
-            ins_amount_flag = true;
+
         }
     }
 
     /*       PREVIOUS INSURANCE NUMBER           */
 
-    if(pre_ins_num == "pre_ins_num" || val=="final_check") {
+    if(val == "pre_ins_num" || val=="final_check") 
+    {
         if(pre_ins_num.value == "") 
         {
             pre_ins_num.style.borderColor = "red";
-            pre_ins_num_flag = false;
+            requirements += "fil the previous insurance number field\n";
+
         }
         else 
         {
             pre_ins_num.style.borderColor = "green";
-            pre_ins_num_flag = true;
+
         }
     }
 
     /*       PREVIOUS INSURANCE ID           */
 
-    if(pre_ins_id == "pre_ins_id" || val=="final_check") {
+    if(val == "pre_ins_id" || val=="final_check") 
+    {
         if(pre_ins_id.value == "") 
         {
             pre_ins_id.style.borderColor = "red";
-            pre_ins_id_flag = false;
+            requirements += "fil the previous insurance id field\n";
         }
         else 
         {
             pre_ins_id.style.borderColor = "green";
-            pre_ins_id_flag = true;
         }
     }
 
     /*       PREVIOUS INSURANCE COMPANY          */
 
-    if(pre_ins_comp == "pre_ins_comp" || val=="final_check") {
+    if(val == "pre_ins_comp" || val=="final_check") 
+    {
         if(pre_ins_comp.value == "") 
         {
             pre_ins_comp.style.borderColor = "red";
-            pre_ins_comp_flag = false;
+            requirements += "fil the previous insurance company field\n";
+
         }
         else 
         {
             pre_ins_comp.style.borderColor = "green";
-            pre_ins_comp_flag = true;
+
         }
     }
 
     /*       COMMENT           */
 
-    if(comment == "comment" || val=="final_check") {
+    if(val == "comment" || val=="final_check") 
+    {
         if(comment.value == "") 
         {
             comment.style.borderColor = "red";
-            comment_flag = false;
+            requirements += "fil the comment field\n";
+
         }
         else 
         {
             comment.style.borderColor = "green";
-            comment_flag = true;
+
         }
     }
     
-
-    fname_flag = true;
-    lname_flag = true;
-    email_flag = true;
-    mobile_flag = true;
-    social_num_flag = true;
-    ins_amount_flag = true;
-    pre_ins_num_flag = true;
-    pre_ins_id_flag = true;
-    pre_ins_comp_flag = true;
-    comment_flag = true;
-
-    final_check_flag = fname_flag && lname_flag && email_flag && mobile_flag && social_num_flag && ins_amount_flag
-    && pre_ins_num_flag && pre_ins_id_flag && pre_ins_comp_flag && comment_flag;
-    
-    return final_check_flag;
+    if (requirements == "")
+    {
+        return true;
     }
+    else
+    {
+        return false;
+    }
+}
