adc0b8efcf580b1eb78aee2bcaedd96defaaf273
Yuda Hatam <yehudahatam@gmail.com>
2021-05-25 17:17:12 +0300

    server db side with new insurance

--- js/SignInScript.js
+++ js/SignInScript.js
@@ -1,15 +1,15 @@
 function loginValidation(){
     var email= document.getElementById("email").value;
     var pass = document.getElementById("pass").value;
-    if(email!="Class"||pass!="1234"){
-        $("#message1-text").html("One of the parameters is incorrect please try again\n "+email+"\n"+pass);
-        $("#exampleModalLabel").html("Failure");
-        $('#exampleModal').modal('show');
-        return
-    }
-    const form= document.createElement('form');
-    form.method='get';
-    form.action='/dashboard';
-    document.body.appendChild(form);
-    form.submit(); 
+    var message ="One of the parameters is incorrect please try again\n mail : "+email+"\n pass : "+pass;
+    $.post('/DB-login',{email:email,pass:pass},function(data,textStatus){
+        if(data=="not ok"){
+            $("#message1-text").html(message.replaceAll('\n','<br>'));
+            $("#exampleModalLabel").html("Failure");
+            $('#exampleModal').modal('show');
+        }
+        else{
+            window.location=data;   
+        }
+    });
 }
