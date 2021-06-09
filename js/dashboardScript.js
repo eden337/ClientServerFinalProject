a8f946ea6a5ddd4e05fbc0bd879e2be5c9e25168
Yuda Hatam <yehudahatam@gmail.com>
2021-06-09 17:41:50 +0300

    changes

--- js/dashboardScript.js
+++ js/dashboardScript.js
@@ -1,6 +1,72 @@
-const fs= require('fs');
+var myTable;
+var severityCell;
+var statucCell;
+var buttonCell;
+var clientName;
+var amount;
+var colIndex;
+var rowIndex;
+function loadTable()
+{
+    $.ajax({
+        url: '/dashboardTable',
+        method: 'get',
+        dataType: 'json',
+        success: function (data){
+            $('#dataTable').on('click', 'td.calc-button', function() {
+                colIndex = myTable.cell(this).index().column;
+                rowIndex = myTable.cell(this).index().row;
+                severityCell=myTable.cell(rowIndex,0);
+                statucCell=myTable.cell(rowIndex,4);
+                buttonCell=myTable.cell(rowIndex,6);
+                clientName=myTable.cell(rowIndex,2).data();
+                amount=myTable.cell(rowIndex,3).data();
+                Calculate();
+              });
+            myTable =  $('#dataTable').DataTable( {
+                    dom: 'Bfrtip',  
+                    data: data,  
+                    rowId:'Button',
+                    buttons:[{
+                        extend: 'create',
+                        name: 'testj2e'
+                    }],                    
+                    columns: [
+                        {data: "severity"},
+                        {data: "category"},
+                        {data: "client_name"},
+                        {data: "insurance_amount"},
+                        {data: "status"},
+                        {data: "due_date"},
+                        {data: "Button",className:"calc-button",defaultContent:'<i class="fas fa-calculator calc-button"></i>'}
+                    ]
+                    
+            }); 
+           
+        }
+    });
+}
+$(document).ready(function() {             
+    loadTable();
+});
+function Calculate(body){
+    $.post('/test',{clientName,amount},function(data,status){
+        console.log(JSON.stringify(data));
+        severityCell.data("<span style='color:red;'>High</span>")
+        statucCell.data("eden");
+        buttonCell.data("<i class='fas fa-ellipsis-h'></i>")
 
-
-function Calculate(){
+    })
+    // var name=myTable.cell(rowIndex,2).data();
+    // var amount=myTable.cell(rowIndex,3).data();
+    // console.log(name);
+    // $.post('/calculate',name,function(data,status){
+    //     console.log(data);    
+    //     myTable.cell(rowIndex,0).data("severe");
+    // })
+    // console.log(name);
+    // if(rowIndex!=null && colIndex!=null){
+    //     myTable.cell(rowIndex, colIndex).data("new");
+    // }
     
 }
--- server.js
+++ server.js
@@ -95,42 +95,96 @@ app.post('/DB-login',urlEncodedParser,function(req,res){
             res.send("not ok")
     });
 })
-app.get('/test', function (req, res) {
-})
-app.get('/yuda', function (req, res) {
-    client.query("insert into requests (client_name,social,email,phone,insurance_amount,previous_insurance_number,previous_insurance_id,previous_insurance_company,comment)"+
-    " values"+" ('"+"name"+"','"+"social"+"','"+"email"+"','"+"mobile"+"','"+"ins_amount"+"','"+"pre_ins_num"+"','"+"pre_ins_id"+"','"+"pre_ins_comp"+"','"+"comment"+"')",function(err,result){
-        res.json(result);
-    });
-})
 
 /*  Need working in local
     Need to check online*/
-app.get('/calculate', function (req, res) {
-    var data = fs.readFileSync('IsraelIsraeli.json')
+app.post('/calculate', function (req, res) {
+    var name=req.body.name;
+    var amount=req.body.amount;
+    console.log(name);
+    var data = fs.readFileSync(name+".json");
     var severity="";
     const UTF8_BOM = "\u{FEFF}";                    
     if( data.includes(UTF8_BOM)){
         data.subarray(1);
     }
     var jsonContent =JSON.parse(data);
-    //res.append("eden","yuda");
-    switch(jsonContent.UserRank){
-        case "1":
-            res.append("eden","yuda1");
-            break;
-        case "2":
-            res.append("eden","yuda2");
-            break;
-        case "3":
-            res.append("eden","yuda3");
-            break;
-        case "4":
-            res.append("eden","yuda4");
-            break;
-    }
-    console.log(jsonContent.UserRank +" "+ res.get("eden"));
+    // //res.append("eden","yuda");
+    // switch(jsonContent.UserRank){
+    //     case "1":
+    //         res.append("eden","yuda1");
+    //         break;
+    //     case "2":
+    //         res.append("eden","yuda2");
+    //         break;
+    //     case "3":
+    //         res.append("eden","yuda3");
+    //         break;
+    //     case "4":
+    //         res.append("eden","yuda4");
+    //         break;
+    // }
+    // console.log(jsonContent.UserRank +" "+ res.get("eden"));
+    return res.json(jsonContent);
 })
+
+app.get('/dashboardTable',function(req,res){
+    client.query("select client_name,insurance_amount,severity,category,status,due_date from requests;",function(err,data){
+        return res.json(data.rows);
+    })
+})
+
+
+app.post('/test',urlEncodedParser, function (req, res) {
+    var clientName=req.body.clientName;
+    var amount=req.body.amount;
+    var severity;
+    var userData = fs.readFileSync(clientName+".json");
+    const UTF8_BOM = "\u{FEFF}";                    
+    if( userData.includes(UTF8_BOM)){
+        userData.subarray(1);
+    }
+    var userJsonContent =JSON.parse(userData);
+
+    var policyData = fs.readFileSync("./json/Policy.json");                  
+    if( policyData.includes(UTF8_BOM)){
+        policyData.subarray(1);
+    }
+    var policyJsonContent =JSON.parse(policyData);
+    var txt="";
+    for(i in policyJsonContent){
+        // txt+=jsonContent[i][0].UserRank;
+        if(policyJsonContent[i][0].UserRank==userJsonContent.UserRank){
+            var id=userJsonContent.insuranceData[0].PrevinsuranceID;
+            severity=i;
+            client.query("UPDATE requests SET severity="+severity+" status='Reviewed' category='Car_Insurance' due_date='1/9/2021' where previous_insurance_id="+id+"");
+        }
+       
+            
+    }
+    console.log(severity);
+    return res.json(userJsonContent);
+})
+app.get('/yuda',urlEncodedParser, function (req, res) {
+    var data = fs.readFileSync("./json/MosheCohen.json");
+    const UTF8_BOM = "\u{FEFF}";                    
+    if( data.includes(UTF8_BOM)){
+        data.subarray(1);
+    }
+    var jsonContent =JSON.parse(data);
+    var txt="";
+    // for(i in jsonContent){
+    //     // txt+=jsonContent[i][0].UserRank;
+    //     if(jsonContent[i][0].UserRank==4)
+    //         txt="yuidad";
+    // }
+    txt=jsonContent.insuranceData[0].companyUserId;
+        
+    res.json(txt);
+})
+
+
+
 
 
 
