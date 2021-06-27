071d7c0f1102a228b63f0ddcb518b36481c853cc
eden337 <eden_337@outlook.com>
2021-06-27 16:10:53 +0300

    remember me

--- js/SignInScript.js
+++ js/SignInScript.js
@@ -1,9 +1,15 @@
+    var rmCheck = document.getElementById("customCheck");
+    var emailInput = document.getElementById("email");
+    var pass = document.getElementById("pass");
+
 function loginValidation(){
     
     var email= document.getElementById("email").value;
-    var pass = document.getElementById("pass").value;
-    var message ="One of the parameters is incorrect please try again\n mail : "+email+"\n pass : "+pass;
-    $.post('/DB-login',{email:email,pass:pass},function(data,textStatus){
+    var currentPass =pass.value;
+    var message ="One of the parameters is incorrect please try again\n mail : "+email+"\n pass : "+currentPass;
+
+    isRememberMe();
+    $.post('/DB-login',{email:email,pass:currentPass},function(data,textStatus){
         if(data==="not ok"){
             $("#message1-text").html(message.replaceAll('\n','<br>'));
             $("#exampleModalLabel").html("Failure");
@@ -13,4 +19,36 @@ function loginValidation(){
             window.location=data;   
         }
     });
+    
 }
+
+
+function isRememberMe(){
+    if (rmCheck.checked && emailInput.value !== "") {
+        localStorage.username = emailInput.value;
+        localStorage.password= pass.value;
+        localStorage.checkbox = rmCheck.value;
+      } else {
+        localStorage.username = "";
+        localStorage.password= "";
+        localStorage.checkbox = "";
+      }
+}
+
+
+function loadRememberMe(){
+    if (localStorage.checkbox && localStorage.checkbox !== "") {
+        rmCheck.setAttribute("checked", "checked");
+        emailInput.value = localStorage.username;
+        pass.value = localStorage.password;
+    } else {
+        rmCheck.removeAttribute("checked");
+        emailInput.value = "";
+        pass.value="";
+
+    }
+}
+
+$(document).ready(function(){
+    loadRememberMe();
+});
